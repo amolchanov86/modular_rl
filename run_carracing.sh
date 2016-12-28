0b44893cab53bb05ab22eeb39a3cccb585c0b2b6
Artem Molchanov <amolchanov86@gmail.com>
2016-12-28 15:04:27 -0800

    car racing iterations decreased to 1000

--- run_carracing.sh
+++ run_carracing.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python ./run_pg.py --env CarRacing-v0 --agent modular_rl.agentzoo.TrpoAgent --outfile results_temp  --n_iter 10000 "$@"
+python ./run_pg.py --env CarRacing-v0 --agent modular_rl.agentzoo.TrpoAgent --outfile results_temp  --n_iter 1000 "$@"
