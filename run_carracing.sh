f0c5539896739614e0f1589bdc55b2d96a84c352
Artem Molchanov <amolchanov86@gmail.com>
2016-12-29 12:50:59 -0800

    snapshoting added

--- run_carracing.sh
+++ run_carracing.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python ./run_pg.py --env CarRacing-v0 --agent modular_rl.agentzoo.TrpoAgent --outfile results_temp  --n_iter 1000 "$@"
+python ./run_pg.py --env CarRacing-v0 --agent modular_rl.agentzoo.TrpoAgent --outfile results_temp  --n_iter 1000 --snapshot_every 1 "$@"
