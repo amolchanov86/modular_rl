07ec404a58dfab54239c38d634ea51e36b142ba2
Artem Molchanov <molchano@usc.edu>
2017-01-07 20:46:15 -0800

    run_carracing: more options added

--- run_carracing.sh
+++ run_carracing.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python ./run_pg.py --env CarRacing-v0 --agent modular_rl.agentzoo.TrpoAgent --outfile results_temp  --n_iter 1000 --snapshot_every 1 "$@"
+python ./run_pg.py --env CarRacing-v0 --agent modular_rl.agentzoo.TrpoAgent --outfile results_temp/exp_current  --n_iter 1000 --snapshot_every 1 --use_hdf 1 "$@"
